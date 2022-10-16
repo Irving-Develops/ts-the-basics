df2d60503ed9d3d6c1405fe6c5f86140b20c5fa4
Irving Arreola Palacios <irving.a789@gmail.com>
2022-10-15 19:07:29 -0500

    union types

--- ts-the-basics/hello.js
+++ ts-the-basics/hello.js
@@ -1,6 +1,21 @@
 // console.log("hello world")
-let msg = "hello world";
+var msg = "hello world";
 function greeting(person, date) {
-    console.log(`hello ${person}, today is ${date.toDateString()}`);
+    console.log("hello ".concat(person, ", today is ").concat(date.toDateString()));
 }
 greeting("irving", new Date());
+//any allows you to use any datatype
+var obj = {
+    x: 0
+};
+obj.foo = "hello";
+obj.func = greeting;
+obj.bool = true;
+// function greet(person: string) {
+//     console.log(`hi ${person}`)
+// }
+//return type goes after parameter
+function getFavNumber() {
+    return "dafh";
+}
+getFavNumber();
--- ts-the-basics/hello.ts
+++ ts-the-basics/hello.ts
@@ -6,3 +6,32 @@ function greeting (person: string, date: Date){
 }
 
 greeting("irving", new Date())
+
+//any allows you to use any datatype
+
+const obj: any = {
+    x: 0
+}
+
+obj.foo = "hello"
+obj.func = greeting
+obj.bool = true
+
+
+
+// function greet(person: string) {
+//     console.log(`hi ${person}`)
+// }
+
+
+//return type goes after parameter
+function getFavNumber(): number {
+    return 7
+}
+
+getFavNumber()
+
+//doesnt work because toUpperCaseUpperCase doesnt exist for numbers
+function printId (id: string | number) {
+    // console.log(id.toUpperCase())
+}
