e074a3cb2a976bd5f33da5efd244db574338df15
Irving Arreola Palacios <irving.a789@gmail.com>
2023-04-10 18:56:34 -0500

    hobbies

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -9,3 +9,14 @@ const number2 = 2.8;
 
 const res = add(number1, number2);
 console.log(res);
+
+
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[] | string;
+} = {
+    name: "irving",
+    age: 24,
+    hobbies: ["sports", "cooking"]
+}
