2945e227783be8c4f0f5a80701831b55773ae4f3
Irving Arreola Palacios <irving.a789@gmail.com>
2023-04-11 17:16:22 -0500

    passing compnents as props

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -14,6 +14,9 @@ import { Box } from "./components/context/Box";
 import { ThemeContextProvider } from "./components/context/ThemeContext";
 import { UserContextProvider } from "./components/context/UserContext";
 import { User2 } from "./components/context/User";
+import { Private } from "./components/auth/Private";
+import { Profile } from "./components/auth/Profile";
+
 
 function App() {
   const personName = {
@@ -46,6 +49,7 @@ function App() {
       <UserContextProvider>
         <User2 />
       </UserContextProvider>
+      <Private isLoggedIn={true} component={Profile} />
     </div>
   );
 }
