2e89759a44998c331245d927916e3d5c9f9cbd8a
Irving Arreola Palacios <irving.a789@gmail.com>
2023-04-12 19:20:19 -0500

    added tailwind and daisyui

---
+++ ts-project/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  
+  return(
+  <div className="App">
+    <header>
+
+    </header>
+    <main>
+      <h2 >test</h2>
+      <button className="btn btn-outline">Button</button>
+    </main>
+    <footer>
+
+    </footer>
+  </div>
+  )
+}
+
+
+export default App;
