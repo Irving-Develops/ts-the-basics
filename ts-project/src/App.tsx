ea999d259f50a170dc1c0e43493c4aafabee75f0
Irving Arreola Palacios <irving.a789@gmail.com>
2023-04-13 22:50:58 -0500

    typescript todolist

--- ts-project/src/App.tsx
+++ ts-project/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuidV4 } from "uuid";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -11,8 +12,8 @@ function App() {
 
     </header>
     <main>
-      <h2 >test</h2>
-      <button className="btn btn-outline">Button</button>
+      <h1 className="p-4">Todo List</h1>
+      <Todo />
     </main>
     <footer>
 
@@ -20,6 +21,69 @@ function App() {
   </div>
   )
 }
+type Task = {
+  uuid: string;
+  name: string;
+  completed: boolean;
+  createdAt: Date;
+}
 
+function Todo () {
+  const [todo, setTodo] = useState<string>("");
+  const [todoList, setTodoList] = useState<Task[]>(() => {
+    const todoList = localStorage.getItem('todoList')
+    if(todoList) {
+      return JSON.parse(todoList)
+    }
+    return []
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }, [todoList])
+
+
+  const addTask = () => {
+
+    if(todo) {
+      let newTask = {
+        uuid: uuidV4(),
+        name: todo,
+        completed: false,
+        createdAt: new Date()
+      }
+      addTaskToList(newTask)
+
+      setTodo("")
+    }
+  }
+
+  const addTaskToList = (task: Task) => {
+    setTodoList([...todoList, task])
+    // localStorage.setItem('todoList', JSON.stringify(todoList))
+  }
+
+  return (
+    <div className="flex gap-2">
+      <input
+        type="text"
+        value={todo}
+        placeholder="Add Item"
+        className="input input-bordered input-info w-full max-w-xs"
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={addTask} className="btn btn-outline">Add</button>
+
+      <div>
+        {todoList.map((task) => (
+          <div key={task.uuid}>
+            <p>{task.name}</p>
+            <input onClick={() => !task.completed} checked={task.completed} type="checkbox" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default App;
